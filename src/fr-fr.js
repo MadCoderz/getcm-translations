f3e5de36a2b22370375f3415eb8beeb1b066fc26
Delphin PETER <delphinpeter@gmail.com>
2013-07-18 20:30:16 +0200

    getcm-translations  : FR translation
    Change-Id: I7f663c132a587f65f96d3d8dcab933808b6a9585

---
+++ src/fr-fr.js
@@ -0,0 +1,17 @@
+(function() {
+  var getcm = window.getcm || { locales:{} };
+
+  _.extend(getcm.locales, { "fr-FR": {
+    navbar: {
+      about:"À propos",
+      download:"Téléchargement",
+      devices:"Appareils",
+      blog:"Blog",
+      forum:"Forum",
+      community:"Communauté",
+      manufacturers:"Fabricants",
+      all_manufacturers:"Tous les fabricants"
+    }
+  }});
+
+}).call(this);
