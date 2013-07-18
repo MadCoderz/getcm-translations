0cc3d1753558ec05b14a8bc7e5c410ea9deab409
Odin Ugedal <odinuge@gmail.com>
2013-07-18 21:01:11 +0200

    getcm-translations: NB translation
    Change-Id: Iede3438c626eb9f97f79712a3136f8f9311fff97

---
+++ src/nb-no.js
@@ -0,0 +1,17 @@
+(function() {
+  var getcm = window.getcm || { locales:{} };
+
+  _.extend(getcm.locales, { "nb-NO": {
+    navbar: {
+      about:"Om",
+      download:"Last ned",
+      devices:"Enheter",
+      blog:"Blogg",
+      forum:"Forum",
+      community:"Felleskap",
+      manufacturers:"Produsenter",
+      all_manufacturers:"Alle Produsenter"
+    }
+  }});
+
+}).call(this);
