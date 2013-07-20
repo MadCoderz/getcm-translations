0d5ac8e3f832d2b9a5ca71481382d5c1fdb3399e
Ondrej Zima <keltuv.email@gmail.com>
2013-07-21 01:22:35 +0200

    getcm-translations: CS translation
    - added Czech translation
    
    Change-Id: Ibf9d69613b7bde0bbda88cf096c05190637a1eb9

---
+++ src/cs-cz.js
@@ -0,0 +1,17 @@
+(function() {
+  var getcm = window.getcm || { locales:{} };
+
+  _.extend(getcm.locales, { "cs-CZ": {
+    navbar: {
+      about:"O aplikaci",
+      download:"Stáhnout",
+      devices:"Zařízení",
+      blog:"Blog",
+      forum:"Fórum",
+      community:"Komunita",
+      manufacturers:"Výrobci",
+      all_manufacturers:"Všichni výrobci"
+    }
+  }});
+
+}).call(this);
