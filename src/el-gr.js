29376b9aa60e47f1909cd8eab34573b45291ed54
Michael Bestas <mikeioannina@gmail.com>
2013-07-22 16:05:51 +0300

    getcm-translations: EL translation
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: Icbe426f668a3137af091caa6c7baf93bb1dc0fdf

---
+++ src/el-gr.js
@@ -0,0 +1,17 @@
+(function() {
+  var getcm = window.getcm || { locales:{} };
+
+  _.extend(getcm.locales, { "el-GR": {
+    navbar: {
+      about:"Σχετικά",
+      download:"Λήψη",
+      devices:"Συσκευές",
+      blog:"Blog",
+      forum:"Forum",
+      community:"Κοινότητα",
+      manufacturers:"Κατασκευαστές",
+      all_manufacturers:"Όλοι οι κατασκευαστές"
+    }
+  }});
+
+}).call(this);
