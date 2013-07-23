066202235271e8a0d34ebca871a66321969df0dc
Andrew Jiang <sam.andrew.jiang@gmail.com>
2013-07-22 21:39:04 -0400

    getcm-translations: Simplified Chinese Translations
    Change-Id: Ic44c5dc8049ee5dc40c824f6994164826f5b309f

---
+++ src/zh-cn.js
@@ -0,0 +1,17 @@
+(function() {
+  var getcm = window.getcm || { locales:{} };
+
+  _.extend(getcm.locales, { "zh-CN": {
+    navbar: {
+      about:"关于",
+      download:"下载",
+      devices:"设备",
+      blog:"博客",
+      forum:"论坛",
+      community:"社区",
+      manufacturers:"厂家",
+      all_manufacturers:"所有厂家"
+    }
+  }});
+
+}).call(this);
