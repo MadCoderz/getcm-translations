a56e67ecfecb072559362b225d71edae3e14c25f
jackmu95 <jacob.mueller.elz@gmail.com>
2013-07-23 19:45:39 +0200

    getcm: Initial German translation
    Change-Id: I70411827c19ec6da087b52de6891123e5a48a033

---
+++ src/de-de.js
@@ -0,0 +1,17 @@
+(function() {
+    var getcm = window.getcm || { locales:{} };
+
+    _.extend(getcm.locales, { "de-DE": {
+        navbar: {
+            about:"Über",
+            download:"Download",
+            devices:"Geräte",
+            blog:"Blog",
+            forum:"Forum",
+            community:"Community",
+            manufacturers:"Hersteller",
+            all_manufacturers:"Alle Hersteller"
+        }
+    }});
+
+}).call(this);
