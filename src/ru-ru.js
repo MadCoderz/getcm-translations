7f00af9223e94356369045bb0397e64ceae0a36f
pvolkov <marshmollow@gmail.com>
2013-08-02 23:18:50 +0400

    get.cm: RU translation
    Change-Id: I1d3a4a8cfde39db64f79719fed193192a4d38d00

---
+++ src/ru-ru.js
@@ -0,0 +1,17 @@
+(function() {
+  var getcm = window.getcm || { locales:{} };
+
+  _.extend(getcm.locales, { "ru-RU": {
+    navbar: {
+      about:"О проекте",
+      download:"Загрузки",
+      devices:"Устройства",
+      blog:"Блог",
+      forum:"Форум",
+      community:"Сообщество",
+      manufacturers:"Производители",
+      all_manufacturers:"Все производители"
+    }
+  }});
+
+}).call(this);
