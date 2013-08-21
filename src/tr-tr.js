1a29661c3b8947969f66058c28b0781317bb535d
MadCoderz <mail223@linuxmail.org>
2013-08-21 21:40:27 +0300

    Turkish translation

---
+++ src/tr-tr.js
@@ -0,0 +1,17 @@
+(function() {
+  var getcm = window.getcm || { locales:{} };
+
+  _.extend(getcm.locales, { "tr-TR": {
+    navbar: {
+      about:"Hakkında",
+      download:"İndir",
+      devices:"Cihazlar",
+      blog:"Blog",
+      forum:"Forum",
+      community:"Topluluk",
+      manufacturers:"Üreticiler",
+      all_manufacturers:"Tüm Üreticiler"
+    }
+  }});
+
+}).call(this);
